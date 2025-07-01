3a60691cbd04f4f42cd3257e86a068ef3e73afb3
Pavel Marchuk <pavel.m@preply.com>
2025-07-01 15:26:17 -0500

    k

--- data.py
+++ data.py
@@ -1,4 +1,6 @@
+import datetime
 import os
+import typing as t
 import yaml
 
 
@@ -9,22 +11,29 @@ with open("config.yaml", 'r') as file:
 PATH = CONFIG["output_path"]
 
 
-def get_daily_metadata():
+def get_daily_metadata(month=None, year=None) -> t.Dict[str, t.Dict[datetime.date, int]]:
     """ Return how many holters each doctor have per day """
     doctors = os.listdir(CONFIG["output_path"])
     data = {}
     for doctor in doctors:
         data[doctor] = {}
         doctor_path = os.path.join(CONFIG["output_path"], doctor)
-        dates = os.listdir(doctor_path)
+        dates = [
+            datetime.datetime.strptime(date_str, "%d.%m.%Y").date()
+            for date_str in os.listdir(doctor_path)
+        ]
         for date in dates:
-            date_path = os.path.join(doctor_path, date)
+            if month is not None and date.month != month:
+                continue
+            if year is not None and date.year != year:
+                continue
+            date_path = os.path.join(doctor_path, date.strftime("%d.%m.%Y"))
             holters = _get_holters_in_folder(date_path)
             data[doctor][date] = len(holters)
     return data
 
 
-def _get_holters_in_folder(folder_path, recursive=False) -> list[str]:
+def _get_holters_in_folder(folder_path, recursive=False) -> t.List[str]:
     """ Return list of full paths to all .zhr files in the folder. """
     if recursive:
         file_names = [
